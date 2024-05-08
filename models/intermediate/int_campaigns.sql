77ddfbbf82bf1968ad00ed6c40e7e2f8c7039387
gautier.baraffe@gmail.com <gautier.baraffe@gmail.com>
2024-05-08 12:21:38 +0000

    Added macros from package

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,3 +1,15 @@
+{{ dbt_utils.union_relations(
+    relations=[
+        ref('stg_raw__adwords'),
+        ref('stg_raw__bing'),
+        ref('stg_raw__facebook'),
+        ref('stg_raw__criteo')
+    ]
+) }}
+
+
+/*
+
 SELECT 
     *
 FROM 
@@ -18,3 +30,4 @@ SELECT
 FROM 
     {{ ref('stg_raw__criteo') }}
 
+*/
