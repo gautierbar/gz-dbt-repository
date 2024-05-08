b54e1db5686500638223dec5574fc97679fa2961
gautier.baraffe@gmail.com <gautier.baraffe@gmail.com>
2024-05-08 10:31:17 +0000

    Added data per month

---
+++ models/mart/finance_campaigns_month.sql
@@ -0,0 +1,12 @@
+SELECT 
+    CONCAT(EXTRACT(MONTH FROM date_date), "-", EXTRACT(YEAR FROM date_date)) AS datemonth
+    ,SUM(nb_transactions) AS nb_transactions
+    ,SUM(revenue) AS revenue 
+    ,SUM(average_basket) AS average_basket
+    ,SUM(margin) AS margin
+    ,SUM(operational_margin) AS operational_margin
+    ,SUM(ads_margin) AS ads_margin
+FROM 
+    {{ ref('finance_campaigns_day') }}
+GROUP BY 
+    datemonth
